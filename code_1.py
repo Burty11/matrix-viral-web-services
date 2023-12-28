519b128aa07d91403f828239783b1d0d77af7023
Burty11 <142454463+Burty11@users.noreply.github.com>
2023-12-28 15:57:59 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,66 @@
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+import random
+
+if __name__ == "__main__":
+    for item in data:
+
+    main()
+def main():
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def main():
+
+
+    return data
+        print(f"Random Number: {item}")
+def generate_random_data():
+import random
+
+
+    main()
+if __name__ == "__main__":
+if __name__ == "__main__":
+
+def generate_random_data():
+
+    main()
+
+    return data
+        print(f"Random Number: {item}")
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    for item in data:
+def main():        print(f"Random Number: {item}")
+
+def main():
+import random
+
+def generate_random_data():
+    main()
+    for item in data:
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+    return datadef main():
+    for item in data:
+    return data
+
+if __name__ == "__main__":
+
+    data = generate_random_data()
+    main()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+import random
+        print(f"Random Number: {item}")
